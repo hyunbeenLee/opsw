9cda6e119094a71ad2abdfec6021e3abf110d9bd
HYUNBIN-LEE\user <hyunbeen95@gmail.com>
2018-05-03 22:35:39 +0900

    하트를 만드는데 필요한 첫 for문

--- 2013244081.c
+++ 2013244081.c
@@ -3,5 +3,13 @@
 int main(void){
   int i, j;
 
+  for(i = 4; i < 10; i += 2 ){
+    for( j = 0; j <= (10 - i); j++ ){printf(" ");}
+		for( j = 0; j <= i * 2; j++ ){printf("*");}
+		for( j = 0; j <= (10 - i) * 2; j++ ){printf(" ");}
+		for( j = 0; j <= i * 2; j++ ){printf("*");}
+		printf("\n");
+  }
+
   return 0;
 }
