8b4693a869ac2b683681005ee042cefb29205f2e
HYUNBIN-LEE\user <hyunbeen95@gmail.com>
2018-05-03 22:36:01 +0900

    하트를 만드는데 필요한 두번째 구문

--- 2013244081.c
+++ 2013244081.c
@@ -10,6 +10,11 @@ int main(void){
 		for( j = 0; j <= i * 2; j++ ){printf("*");}
 		printf("\n");
   }
+  for( i = 20; i >= 0; i -= 2 ){
+		for( j = 0;  j <= 20 - i; j++ ){ printf(" ");}
+		for( j = 0; j <= i * 2; j++ ){printf("*");}
+		printf("\n");
+	}
 
   return 0;
 }
